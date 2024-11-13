7a331b899c587faacf43ba64ab7feaa40530876a
David Golke <golkedj@gmail.com>
2024-11-13 00:42:33 -0600

    Fix invalid duration bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Results from "./components/Results";
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 15000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
@@ -13,20 +14,18 @@ function App() {
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
-      return { ...prevUserInput, [inputIdentifier]: +newValue };
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
     });
   }
-
-  const inputIsValid = userInput.duration >= 1;
 
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      {!inputIsValid && (
-        <p className="center">Please enter a duration greater than zero.</p>
-      )}
-      {inputIsValid && <Results input={userInput} />}
+      <Results input={userInput} />
     </>
   );
 }
